07853c935dcc9e1fcc4c71466ae587b79e0c4fe6
Lachlan Alphy Robert Peake <l.peake@se23.qmul.ac.uk>
2026-05-02 18:30:47 +0100

    Update test_twilio_auth.py

--- tests/test_twilio_auth.py
+++ tests/test_twilio_auth.py
@@ -6,7 +6,7 @@ import os
 env_path = '/home/pibridge/meshcore-emergency-bridge/.env'
 load_dotenv(dotenv_path=env_path)
 auth_token = os.getenv('TWILIO_AUTH_TOKEN')
-
+account_sid = "AC525f8411a5a1219325127c185d9a3bc6"
 
 client = Client(account_sid, auth_token)
 # Just try to list accounts – this will fail if auth is wrong
