6c5d2032895744866af792c07c3a8fa023a6cef1
Lachlan Alphy Robert Peake <l.peake@se23.qmul.ac.uk>
2026-05-05 23:03:26 +0100

    Update config.py

--- bridge/config.py
+++ bridge/config.py
@@ -7,7 +7,7 @@ SERIAL_PORT = '/dev/ttyUSB0'
 DB_PATH = '/home/pibridge/meshcore-emergency-bridge/data.db'
 
 # External forwarding (set to None if not used/ not currently testing)
-FORWARD_HTTP_URL = None      # change back toNone
+FORWARD_HTTP_URL ="https://webhook.site/5a582fd1-81c9-4489-8543-e31819d55dec"
 
 FORWARD_SMS_TO = "whatsapp:+447436840986"         
 TWILIO_ACCOUNT_SID = "AC525f8411a5a1219325127c185d9a3bc6"
